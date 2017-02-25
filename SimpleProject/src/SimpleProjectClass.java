9d1ec2dee9698182881034ef926bcd49b2719a56
bharat1097 <bharat1097@yahoo.com>
2017-02-25 14:01:58 -0500

    Commit# 2

--- SimpleProject/src/SimpleProjectClass.java
+++ SimpleProject/src/SimpleProjectClass.java
@@ -3,6 +3,7 @@ public class SimpleProjectClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World, this is a message from the SimpleProjectClass");
+		System.out.println("This is the additional line for step number 4 of the exercise. The sum of 5 + 5 = 10");
 
 	}
 
